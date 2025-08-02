ff9c74d1703be4cba00ba72b8e969e8a287d5996
sujalnaik12 <141345646+sujalnaik12@users.noreply.github.com>
2025-08-02 22:27:27 +0530

    grade_calculator_v2.py

--- grade_calculator_v2.py
+++ grade_calculator_v2.py
@@ -1,14 +1,29 @@
-def calculate_average(a,b,c):
-  return (a + b + c / 2)
+def calculate_average(a, b, c):
+  return (a + b + c / 3)
 
-def total_marks(a,b,c):
-  return (a+b+c)
 
-maths = float(input("Enter your Maths marks: "))
-science = float(input("Enter your Science marks: "))
-english = float(input("Enter your English marks: "))
+def total_marks(a, b, c):
+  return (a + b + c)
+
+
+def get_valid_marks(subject):
+  while True:
+    try:
+      marks = float(input(f"Enter you {subject} marks(0-100): "))
+      if 0 <= marks <= 100:
+        return marks
+      else:
+        print("Marks must be between 0 and 100, Try again!")
+    except ValueError:
+      print("Invalid input, please enter a number.")
+
+
+maths = get_valid_marks("Maths")
+science = get_valid_marks("Science")
+english = get_valid_marks("English")
 
 average = calculate_average(maths, science, english)
+
 
 def calculate_grade(average):
   if average >= 90:
@@ -19,6 +34,7 @@ def calculate_grade(average):
     return 'C'
   else:
     return 'D'
+
 
 print(f"Average marks: {average}")
 print(f"Total marks: {total_marks(maths,science,english)}")
